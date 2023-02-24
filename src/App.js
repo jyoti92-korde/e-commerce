4c4b1d54c782d2a4da33a167b404ce3a65f69a99
jyoti92-korde <117429213+jyoti92-korde@users.noreply.github.com>
2023-02-24 20:24:03 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './redux/store';
+import Products from './component/Product';
+import Details from './component/Details';
+import EditProduct from './component/EditProduct';
+import AddProduct from './component/AddProduct';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Provider store={store}>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<Products />}></Route>
+                        <Route path="/Details/:id" element={<Details />}></Route>
+                        <Route path="/AddProduct" element={<AddProduct />}></Route>
+                        <Route path="/EditProduct/:id" element={<EditProduct />}></Route>
+                    </Routes>
+                </BrowserRouter>
+            </Provider>
+        </div>
+    );
 }
 
 export default App;
